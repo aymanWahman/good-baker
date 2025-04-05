7c4d8b8019c26a10b4ab8d223a426ac0f4fae59c
AymanWahman <aymanwahman80@gmail.com>
2025-04-05 17:13:31 +0200

    first change

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,21 @@
-// lib/prisma.ts
+import { Environments } from '@/constants/enums';
 import { PrismaClient } from '@prisma/client';
 
-// تهيئة العميل
-const prisma = new PrismaClient();
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
 
-// تصديره لاستخدامه في المشروع
-export { prisma };
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === Environments.DEV
+        ? ['query', 'error', 'warn']
+        : ['error'],
+  });
+
+if (process.env.NODE_ENV !== Environments.PROD) globalForPrisma.prisma = db;
